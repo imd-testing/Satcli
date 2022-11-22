93262bc85d2d9d11fc294fe5346272dd16f3244e
imd-testing <83472996+imd-testing@users.noreply.github.com>
2022-11-22 17:56:34 +0100

    Ajout d'un docker compose et d'images mariadb et elasticsearch

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 Un projet de formation.
 
+## Installation
+
+Créer une fichier .env à la racine de répertoire, et définisez y un mot de passe.
+
+example: 
+
+```bash
+# dans le fichier .env 
+
+MARIADB_PASSWORD=<your_secret>
+```
+
 ## Liste des API steam
 
 - Détails d'une application https://store.steampowered.com/api/appdetails?appids=730
--- satcli/mariadb.py
+++ satcli/mariadb.py
@@ -6,36 +6,42 @@ class Mariadb():
     def __init__(self):
         self.con = None
         self.cursor = None
+        
+        self.base_dir = os.path.dirname(os.path.realpath(__file__))
+        self.env = self.read_env()
+        self.open()
 
-        try:
-            self.open()
-        except Connector.errors.ProgrammingError:
-            self.create_database()
-            self.open()
-    
+    def read_env(self):
+        config = {}
+
+        dotenv = os.path.join(self.base_dir, '../.env')
+        if os.path.exists(dotenv):
+            with open(dotenv) as file:
+                content = file.read().split("\n")
+
+                for line in content:
+                    parsed = line.split('=')
+                    if len(parsed) == 2:
+                        config[parsed[0].strip()] = parsed[1].strip()
+
+        return config
+
     def open(self):
+        port = self.env.get('MARIADB_PORT')
+        if port is None:
+            port = 3306
+            
+        password = self.env.get('MARIADB_PASSWORD')
+        if password is None:
+            raise 'You need to define a MARIADB_PASSWORD in a .env file at your project root.'
+        
         self.con = Connector.connect(
             host = 'localhost',
-            user = getpass.getuser(),
-            database = 'steampowered',
-            unix_socket = '/run/mysqld/mysqld.sock'
+            user = 'steampowered',
+            password = password,
+            port = port,
+            database = 'steampowered'
         )
-        
-    def create_database(self):
-        self.con = Connector.connect(
-            host = 'localhost',
-            user = getpass.getuser(),
-            database = 'mysql',
-            unix_socket = '/run/mysqld/mysqld.sock'
-        )
-        
-        cursor = self.con.cursor()
-        
-        base_dir = os.path.dirname(os.path.realpath(__file__))
-        
-        with open(os.path.join(base_dir, 'schema.sql')) as file:
-            for query in file.read().split(";"):
-                cursor.execute(query)
-                
+
     def close(self):
         self.con.close()
