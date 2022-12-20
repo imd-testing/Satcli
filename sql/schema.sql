37d5ee3c69c8bb25bd4d9b08b5da7ec1f96b77c3
imd-testing <83472996+imd-testing@users.noreply.github.com>
2022-12-20 11:33:19 +0100

    :fix: correction schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -43,50 +43,14 @@ CREATE TABLE IF NOT EXISTS steampowered.genres (
 	KEY ( label )
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS steampowered.app_2_developers (
-	id int unsigned auto_increment,
-	id_app int unsigned,
-	id_developers int unsigned,
-	PRIMARY KEY ( id ),
-	CONSTRAINT `fk_app_list` FOREIGN KEY (id_app) REFERENCES app_list (id) ON DELETE CASCADE ON UPDATE RESTRICT,
-	CONSTRAINT `fk_developers` FOREIGN KEY (id_developers) REFERENCES developers (id) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS steampowered.app_2_publishers (
-	id int unsigned auto_increment,
-	id_app int unsigned,
-	id_publishers int unsigned,
-	PRIMARY KEY ( id ),
-	CONSTRAINT `fk_app_list` FOREIGN KEY (id_app) REFERENCES app_list (id) ON DELETE CASCADE ON UPDATE RESTRICT,
-	CONSTRAINT `fk_publishers` FOREIGN KEY (id_publishers) REFERENCES publishers (id) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS steampowered.app_2_categories (
-	id int unsigned auto_increment,
-	id_app int unsigned,
-	id_categories int unsigned,
-	PRIMARY KEY ( id ),
-	CONSTRAINT `fk_app_list` FOREIGN KEY (id_app) REFERENCES app_list (id) ON DELETE CASCADE ON UPDATE RESTRICT,
-	CONSTRAINT `fk_categories` FOREIGN KEY (id_categories) REFERENCES categories (id) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS steampowered.app_2_genres (
-	id int unsigned auto_increment,
-	id_app int unsigned,
-	id_genres int unsigned,
-	PRIMARY KEY ( id ),
-	CONSTRAINT `fk_app_list` FOREIGN KEY (id_app) REFERENCES app_list (id) ON DELETE CASCADE ON UPDATE RESTRICT,
-	CONSTRAINT `fk_genres` FOREIGN KEY (id_genres) REFERENCES genres (id) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS steampowered.app_list (
+CREATE TABLE IF NOT EXISTS steampowered.applications (
 	id int unsigned,
 	id_controller_support int unsigned,
 
 	app_name varchar(2028),
 	required_age int unsigned,
 	metacritic_score int unsigned,
-	recommendations_count int unsigned
+	recommendations_count int unsigned,
 
 	release_date date,
 
@@ -95,6 +59,42 @@ CREATE TABLE IF NOT EXISTS steampowered.app_list (
 	is_available_on_linux tinyint unsigned,
 	is_free tinyint unsigned,
 
-	PRIMARY_KEY ( id ),
-	CONSTRAINT `fk_controller_support` FOREIGN KEY (id_controller_support) REFERENCES controller_support (id) ON DELETE CASCADE ON UPDATE RESTRICT
+	PRIMARY KEY ( id ),
+	CONSTRAINT `fk_applications_controller_support` FOREIGN KEY (id_controller_support) REFERENCES controller_support (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS steampowered.app_2_developers (
+	id int unsigned auto_increment,
+	id_app int unsigned,
+	id_developers int unsigned,
+	PRIMARY KEY ( id ),
+	CONSTRAINT `fk_a2d_applications` FOREIGN KEY (id_app) REFERENCES applications (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	CONSTRAINT `fk_a2d_developers` FOREIGN KEY (id_developers) REFERENCES developers (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS steampowered.app_2_publishers (
+	id int unsigned auto_increment,
+	id_app int unsigned,
+	id_publishers int unsigned,
+	PRIMARY KEY ( id ),
+	CONSTRAINT `fk_a2p_applications` FOREIGN KEY (id_app) REFERENCES applications (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	CONSTRAINT `fk_a2p_publishers` FOREIGN KEY (id_publishers) REFERENCES publishers (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS steampowered.app_2_categories (
+	id int unsigned auto_increment,
+	id_app int unsigned,
+	id_categories int unsigned,
+	PRIMARY KEY ( id ),
+	CONSTRAINT `fk_a2c_applications` FOREIGN KEY (id_app) REFERENCES applications (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	CONSTRAINT `fk_a2c_categories` FOREIGN KEY (id_categories) REFERENCES categories (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS steampowered.app_2_genres (
+	id int unsigned auto_increment,
+	id_app int unsigned,
+	id_genres int unsigned,
+	PRIMARY KEY ( id ),
+	CONSTRAINT `fk_a2g_applications` FOREIGN KEY (id_app) REFERENCES applications (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+	CONSTRAINT `fk_a2g_genres` FOREIGN KEY (id_genres) REFERENCES genres (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
