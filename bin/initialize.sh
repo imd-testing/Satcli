6ae1a1be27f5905a8237e3e327313e832c98bba5
imd-testing <83472996+imd-testing@users.noreply.github.com>
2023-05-04 14:34:56 +0200

    🌟 initialize rec-créer une nouvelle base pgsql

--- bin/initialize.sh
+++ bin/initialize.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 set -eEu
+set -o pipefail
 
-export BASE_DIR=$( realpath $( dirname "${BASH_SOURCE[0]}" )/.. )
-
+BASE_DIR=$( realpath $( dirname "${BASH_SOURCE[0]}" )/.. )
+PROJECT_NAME=$( basename "$BASE_DIR")
+VOLUME_NAME="${PROJECT_NAME}_pgdata"
 COMPOSE="docker-compose -f $BASE_DIR/docker-compose.initialize.yml --project-directory $BASE_DIR"
+
+if docker volume ls -q | grep -q "$VOLUME_NAME"
+then
+	echo "VOLUME $VOLUME_NAME, deleting."
+	$COMPOSE down
+	docker volume rm $VOLUME_NAME
+fi
 
 $COMPOSE up --build --abort-on-container-exit
